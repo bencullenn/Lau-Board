9b5c21f877f0c4361c0ce80a5315bb6291ab2eca
Ben Cullen <bencullenn@gmail.com>
2017-06-08 19:15:19 -0400

    Added sound files, and made them successfully play

--- Lau Board/CollectionViewController.swift
+++ Lau Board/CollectionViewController.swift
@@ -11,11 +11,43 @@ import AVFoundation
 
 class CollectionViewController: UICollectionViewController {
 
-    var soundPlayer = AVAudioPlayer()
+    var audioPlayer = AVAudioPlayer()
+    var soundReferences = [String: String] ()
+    var soundTitles = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        soundReferences = ["Alrigth":"allright",
+         "Any Questions?":"anyquestions",
+         "AYE":"aye",
+         "Can Do Better":"candobetter",
+         "It's Doable":"doable",
+         "Cup of Java":"java",
+         "Lunchtime":"lunch",
+         "I'm Moses":"moses",
+         "OHH":"ohh",
+         "OH NO!":"ohno",
+         "OK":"ok",
+         "Put away Cell Phone":"putawayyourcellphone",
+         "See Me":"seemepls",
+         "VERY Doable":"verydoable"]
+        
+        soundTitles = ["Alrigth",
+                       "Any Questions?",
+                       "AYE",
+                       "Can Do Better",
+                       "It's Doable",
+                       "Cup of Java",
+                       "Lunchtime",
+                       "I'm Moses",
+                       "OHH",
+                       "OH NO!",
+                       "OK",
+                       "Put away Cell Phone",
+                       "See Me",
+                       "VERY Doable"]
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,16 +56,39 @@ class CollectionViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return soundTitles.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
         
+        let button: UIButton = cell.viewWithTag(1) as! UIButton
+        
+        button.setTitle(soundTitles[indexPath.row], for: UIControlState.normal)
+        
         return cell
     }
-    @IBAction func playSound(_ sender: Any) {
-       
-    }
-}
+    @IBAction func playSound(_ sender: UIButton) {
+        do {
+        //Create audio player with sound file
+        audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: soundReferences[(sender.titleLabel?.text)!], ofType:"wav")!))
+        
+        audioPlayer.prepareToPlay()
+        // Create an audio session
+            do {
+                var audioSession = AVAudioSession.sharedInstance()
 
+                try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+            }catch{
+               print (error)
+            }
+        
+        // Play sound using audio player
+        audioPlayer.play()
+        }
+        catch {
+            print (error)
+        }
+   }
+
+}
